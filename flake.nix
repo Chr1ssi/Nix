09b9aa9fc71e4c3a57d5bcb1be2bd5e7d5caa7ba
NixOS VM <nixos-vm@local>
2026-09-12 05:19:30 +0200

    Initial NixOS flake configuration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+	description = "NixOS VM configuration";
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+	outputs = { nixpkgs, ...}:
+	{
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+
+			modules = [
+				./configuration.nix
+			];
+		};
+	};
+}
