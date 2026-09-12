95d19fd249a56dad39bf23d0055c07ee4715760e
NixOS VM <nixos-vm@local>
2026-09-12 05:33:29 +0200

    Add Home Manager for chris

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
 	description = "NixOS VM configuration";
-	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-	outputs = { nixpkgs, ...}:
+		home-manager = {
+			url = "github:nix-community/home-manager/release-26.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { nixpkgs, home-manager, ...}:
 	{
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 
 			modules = [
 				./configuration.nix
+				home-manager.nixosModules.home-manager
+
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.chris = import ./home.nix;
+				}
 			];
 		};
 	};
---
+++ home.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+	home.username = "chris";
+	home.homeDirectory = "/home/chris";
+
+	home.stateVersion = "26.05";
+
+	programs.home-manager.enable = true;
+}
