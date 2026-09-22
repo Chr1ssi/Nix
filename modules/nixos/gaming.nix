6b1cec75f822fbb988ee8f59ec90566348254ad4
NixOS VM <nixos-vm@local>
2026-09-22 23:56:48 +0200

    add gamemode and mangohub to gaming module

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -11,10 +11,13 @@
       hardware.graphics.enable32Bit = true;
       services.pipewire.alsa.support32Bit = true;
 
+      programs.gamemode.enable = true;
+
       home-manager.users.chris = {
         home.packages = with pkgs; [
           heroic
           vesktop
+          mangohub
         ];
 
         xdg.configFile."vesktop/settings.json".source =
