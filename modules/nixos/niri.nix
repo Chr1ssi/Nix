57d5095a35742bc43f8dc7bd20c5b3857b905958
NixOS VM <nixos-vm@local>
2026-09-22 20:42:57 +0200

    fix nixos/niri.nix empty

---
+++ modules/nixos/niri.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  flake.modules.nixos.niri =
+    { pkgs, ... }:
+    {
+      programs.niri.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
+      ];
+    };
+}
