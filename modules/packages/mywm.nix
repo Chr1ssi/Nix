9dcbe0e14c174456c8abaa1fcc3b32dc67f6ba10
NixOS VM <nixos-vm@local>
2026-09-22 19:19:32 +0200

    fix mywm build package

--- modules/packages/mywm.nix
+++ modules/packages/mywm.nix
@@ -1,19 +1,55 @@
-{ inputs, ... }:
+{ lib, rustPlatform, makeWrapper, src, shellSrc, quickshell, libxkbcommon,
+  swaylock, swayidle, wlopm, systemd }:
 
-{
-  perSystem = { pkgs, system, ... }:
-    let
-      mywm = pkgs.callPackage ../../packages/mywm.nix {
-        src = inputs.mywm-src;
-        shellSrc = inputs.mywm-shell-src;
-      };
-    in
-    {
-      packages = {
-        inherit mywm;
-        default = mywm;
-      };
+rustPlatform.buildRustPackage {
+  pname = "mywm";
+  version = "0.1.0";
 
-      _module.args.mywm = mywm;
-    };
+  src = lib.cleanSourceWith {
+    inherit src;
+
+    filter = path: type:
+      !(builtins.elem
+        (builtins.baseNameOf path)
+        [ ".git" "target" "quickshell" ]);
+  };
+
+  cargoLock.lockFile = src + "/Cargo.lock";
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  nativeCheckInputs = [
+    libxkbcommon
+  ];
+
+  postPatch = ''
+    substituteInPlace src/river.rs \
+      --replace-fail /usr/share/river-protocols/stable ${./river-protocols}
+  '';
+
+  postInstall = ''
+    mkdir -p $out/share/mywm/quickshell
+    cp -r ${shellSrc}/quickshell/. $out/share/mywm/quickshell/
+
+    wrapProgram $out/bin/mywm \
+      --set-default MYWM_SHELL_DIR $out/share/mywm/quickshell \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          quickshell
+          libxkbcommon
+          swaylock
+          swayidle
+          wlopm
+          systemd
+        ]
+      }
+  '';
+
+  meta = {
+    description = "Custom River window manager with its Quickshell UI";
+    mainProgram = "mywm";
+    platforms = lib.platforms.linux;
+  };
 }
